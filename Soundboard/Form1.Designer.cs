ac9b691c9c4818b03edd8978b7c15a688c522bb4
prodg <prodg@DESKTOP-TRS22OM>
2018-08-02 21:36:26 -0700

    Add project files.

---
+++ Soundboard/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Soundboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newKeyBind = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // newKeyBind
+            // 
+            this.newKeyBind.Location = new System.Drawing.Point(197, 244);
+            this.newKeyBind.Name = "newKeyBind";
+            this.newKeyBind.Size = new System.Drawing.Size(138, 80);
+            this.newKeyBind.TabIndex = 0;
+            this.newKeyBind.Text = "Add New Keybind";
+            this.newKeyBind.UseVisualStyleBackColor = true;
+            this.newKeyBind.Click += new System.EventHandler(this.newKeyBind_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 549);
+            this.Controls.Add(this.newKeyBind);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newKeyBind;
+    }
+}
+
---
+++ Soundboard/Key.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soundboard
+{
+    public class Key
+    {
+        public string PathToSound { get; set; }
+        public Keys KeyToPress { get; set; }
+
+        public Key(string pathToSound, Keys key)
+        {
+            PathToSound = pathToSound;
+            KeyToPress = key;
+        }
+    }
+}
